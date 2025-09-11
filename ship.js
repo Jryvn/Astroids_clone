89976009d99026d0e283744c96e1377a4c7fc1cf
Bill Adams <jryvn@yahoo.com>
2025-09-11 18:51:45 -0400

    Moved ship code to ship.js

--- ship.js
+++ ship.js
@@ -1,34 +1,90 @@
-function Ship ()
-{
-  this.x = 0;
-  this.y = 0;
-  this.width = 25;
-  this.height = 20;
-  this.rotation = 0;
-  this.showFlame = false;
-}
+"use strict";
+class Ship {
+  vr = 0;
+  thrust = 0;
+  rotation = 0;
+  x = 0;
+  y = 0;
+  width = 25;
+  height = 20;
+  showFlame = false;
+  vx = 0;
+  vy = 0;
 
-Ship.prototype.draw = function (context)
-{
-  context.save();
-  context.translate(this.x, this.y);
-  context.rotate(this.rotation);
-  context.lineWidth = 1;
-  context.strokeStyle = "#ffffff";
-  context.beginPath();
-  context.moveTo(10, 0);
-  context.lineTo(-10, 10);
-  context.lineTo(-5, 0);
-  context.lineTo(-10, -10);
-  context.lineTo(10, 0);
-  context.stroke();
-  if (this.showFlame)
-  {
-    context.beginPath();
-    context.moveTo(-7.5, -5);
-    context.lineTo(-15, 0);
-    context.lineTo(-7.5, 5);
-    context.stroke();
+  constructor(canvas) {
+    this.top = 0;
+    this.left = 0;
+    this.right = canvas.width;
+    this.bottom = canvas.bottom;
   }
-  context.restore();
-};
+
+  update() {
+    //console.log("update VR: " + this.vr);
+    this.rotation += (this.vr * Math.PI) / 180;
+    const ax = Math.cos(this.rotation) * this.thrust;
+    const ay = Math.sin(this.rotation) * this.thrust;
+    const friction = 0.99;
+
+    this.vx += ax;
+    this.vy += ay;
+    this.vx *= friction;
+    this.vy *= friction;
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.x - this.width / 2 > this.right) {
+      this.x = this.left - this.width / 2;
+    } else if (this.x + this.width / 2 < this.left) {
+      this.x = this.right + this.width / 2;
+    }
+    if (this.y - this.height / 2 > this.bottom) {
+      this.y = this.top - this.height / 2;
+    } else if (this.y + this.height / 2 < top) {
+      this.y = this.bottom + this.height / 2;
+    }
+  }
+
+  draw(context) {
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.rotation);
+    context.lineWidth = 1;
+    context.strokeStyle = "#ffffffff";
+    context.beginPath();
+    context.moveTo(10, 0);
+    context.lineTo(-10, 10);
+    context.lineTo(-5, 0);
+    context.lineTo(-10, -10);
+    context.lineTo(10, 0);
+    context.stroke();
+    if (this.showFlame) {
+      context.beginPath();
+      context.moveTo(-7.5, -5);
+      context.lineTo(-15, 0);
+      context.lineTo(-7.5, 5);
+      context.stroke();
+    }
+    context.restore();
+  }
+
+  keyPressed = (event) => {
+    switch (event.keyCode) {
+      case 37: // left
+        this.vr = -3;
+        break;
+      case 39: // right
+        this.vr = 3;
+        break;
+      case 38: // up
+        this.thrust = 0.05;
+        this.showFlame = true;
+        break;
+    }
+    //console.log("event VR: " + this.vr);
+  }
+
+  keyReleased = (event) => {
+    this.vr = 0;
+    this.thrust = 0;
+    this.showFlame = false;
+  }
+}
